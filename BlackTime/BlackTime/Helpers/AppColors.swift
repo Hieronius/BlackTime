3bb098687162701c18c0093c26b74fda21ecbad5
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-12 08:05:25 +0300

    Add Main colors of the app

---
+++ BlackTime/BlackTime/Helpers/AppColors.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+enum AppColors {
+
+	static let primaryColor = UIColor.init(hex: "#000000")
+	static let secondaryColor = UIColor.init(hex: "#7D7D7D")
+	static let actionColor = UIColor.init(hex: "#FFFFFF")
+}
