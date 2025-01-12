5dbc6f3714b8f181a70a79ac6b47b7926dadf50d
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-12 07:12:20 +0300

    Setup project structure

---
+++ BlackTime/BlackTime/Documentation/ReadMe.md
@@ -0,0 +1,8 @@
+#  ToDoList
+
+1. Setup SnapKit
+2. Setup Project structure
+3. Get GenericViewController
+4. Get colors for the app
+5. Create Authorisation Screen
+
---
+++ BlackTime/BlackTime/Resources/SceneDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  SceneDelegate.swift
+//  BlackTime
+//
+//  Created by Арсентий Халимовский on 12.01.2025.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+	var window: UIWindow?
+
+
+	func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+		guard let windowScene = (scene as? UIWindowScene) else { return }
+
+		window = UIWindow(windowScene: windowScene)
+
+		guard let window else { return }
+
+		let rootVC = AuthorisationViewController()
+		rootVC.view.backgroundColor = .blue
+
+		window.rootViewController = rootVC
+		window.makeKeyAndVisible()
+	}
+
+	func sceneDidDisconnect(_ scene: UIScene) {
+
+	}
+
+	func sceneDidBecomeActive(_ scene: UIScene) {
+
+	}
+
+	func sceneWillResignActive(_ scene: UIScene) {
+
+	}
+
+	func sceneWillEnterForeground(_ scene: UIScene) {
+
+	}
+
+	func sceneDidEnterBackground(_ scene: UIScene) {
+
+	}
+
+
+}
+
