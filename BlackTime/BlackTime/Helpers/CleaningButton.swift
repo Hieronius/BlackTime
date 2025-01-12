0dfee5c1a3d1a12f9b4129358f8dbf60fb676377
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-12 08:18:10 +0300

    Add custom cleaning button

---
+++ BlackTime/BlackTime/Helpers/CleaningButton.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+/// Custom cleaning button for any text fields where you need to remove some text.
+final class CleaningButton: UIButton {
+
+	// MARK: - Initializers
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		setupCleaningButton()
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - Private Methods
+
+	private func setupCleaningButton() {
+		setImage(UIImage(systemName: "delete.left.fill"), for: .normal)
+		tintColor = .black
+		widthAnchor.constraint(equalToConstant: 30).isActive = true
+		addTarget(self, action: #selector(clearTextField(sender: )), for: .touchUpInside)
+	}
+
+	@objc private func clearTextField(sender: UIButton) {
+		if let textField = sender.superview as? UITextField {
+			textField.text = ""
+		}
+	}
+
+
+}
