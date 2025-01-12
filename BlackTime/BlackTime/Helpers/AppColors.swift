d1cb2dc1f7c14295abffcfcacbeda775d804df6f
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-12 09:01:45 +0300

    Add documentation

--- BlackTime/BlackTime/Helpers/AppColors.swift
+++ BlackTime/BlackTime/Helpers/AppColors.swift
@@ -1,8 +1,14 @@
 import UIKit
 
+/// Enum to store all colors which been used in the App
 enum AppColors {
 
+	/// Main Color for background and text
 	static let primaryColor = UIColor.init(hex: "#000000")
+
+	/// Secondary color for subviews and contrainers
 	static let secondaryColor = UIColor.init(hex: "#7D7D7D")
+
+	/// Main color for buttons, actions, menues
 	static let actionColor = UIColor.init(hex: "#FFFFFF")
 }
