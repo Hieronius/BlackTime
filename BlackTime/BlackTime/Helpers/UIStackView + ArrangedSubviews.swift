b786ab434298e28310759f97ded66eb99031f9d8
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-17 06:30:40 +0300

    add addArrangedSubviews extension

---
+++ BlackTime/BlackTime/Helpers/UIStackView + ArrangedSubviews.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+extension UIStackView {
+
+	/// Adds multiple arranged subviews to the stack view.
+	///
+	/// - Parameter views: An array of views to be added as arranged subviews.
+	func addArrangedSubviews(_ views: [UIView]) {
+		views.forEach { self.addArrangedSubview($0) }
+	}
+}
