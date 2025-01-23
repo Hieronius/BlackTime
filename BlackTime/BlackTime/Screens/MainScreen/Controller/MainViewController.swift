19b0c42003aa0980a8e35e804cb547116313b9af
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-23 07:11:06 +0300

    Create initial MainViewController

---
+++ BlackTime/BlackTime/Screens/MainScreen/Controller/MainViewController.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+/// `Main Screen` to display current day's statistic and existing time to spend
+final class MainViewController: GenericViewController<MainRootView> {
+
+	// MARK: - Initialization
+
+	init() {
+		super.init(nibName: nil, bundle: nil)
+		rootView.backgroundColor = AppColors.primaryColor
+
+		// pass dependencies there
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - Life Cycle
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		setupNavigationBar()
+	}
+}
+
+// MARK: - Setup Navigation Bar
+
+private extension MainViewController {
+
+	func setupNavigationBar() {
+
+		let sandwichMenuButton = UIBarButtonItem(
+
+			image: UIImage(systemName: "line.3.horizontal"),
+			style: .plain,
+			target: self,
+			action: #selector(toggleMenu)
+		)
+
+		sandwichMenuButton.tintColor = AppColors.actionColor
+		navigationItem.leftBarButtonItem = sandwichMenuButton
+
+		let calendarButton = UIBarButtonItem(
+
+			image: UIImage(systemName: "calendar"),
+			style: .plain,
+			target: self,
+			action: #selector(toggleCalendar)
+
+		)
+
+		calendarButton.tintColor = AppColors.actionColor
+		navigationItem.rightBarButtonItem = calendarButton
+	}
+
+	@objc func toggleMenu() {
+
+		// Open-close sandwich menu
+	}
+
+	@objc func toggleCalendar() {
+
+		// Open-close Calendar
+	}
+
+	// actions for buttons
+
+}
