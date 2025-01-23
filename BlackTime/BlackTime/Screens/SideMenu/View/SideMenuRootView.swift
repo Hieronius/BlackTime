3d5847f7a4df2f52f23562d1e10846a7dbc392e5
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-23 08:33:04 +0300

    Create initial SideMenuRootView

---
+++ BlackTime/BlackTime/Screens/SideMenu/View/SideMenuRootView.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+/// `rootView` for SideMenuViewController
+final class SideMenuRootView: UIView {
+
+	// Options
+	// Statistics
+	// History
+	// Categories
+	// Profile?
+	// Premium?
+
+	// MARK: - Initialization
+
+	init() {
+		super.init(frame: .zero)
+		backgroundColor = .white
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("NSCoder has not been implemented")
+	}
+}
