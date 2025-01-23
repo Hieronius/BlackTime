5cd0b844857829366e7a57b0cab6071c208efc2c
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-23 08:32:52 +0300

    Create initial SideMenuViewController

---
+++ BlackTime/BlackTime/Screens/SideMenu/Controller/SideMenuViewController.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+/// Controller to manage `SideMenu` content
+final class SideMenuViewController: GenericViewController<SideMenuRootView> {
+
+	// MARK: - Initialization
+
+	init() {
+		super.init(nibName: nil, bundle: nil)
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	// MARK: - Life Cycle
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
