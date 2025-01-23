d83dc74096fdb99d3144b94acd384db63b687aa4
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-23 10:01:11 +0300

    Add time category model

---
+++ BlackTime/BlackTime/Screens/Main/Model/TimeCategory.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+/// struct to track time categories and their durations:
+struct TimeCategory {
+	let name: String
+	let color: UIColor
+	var hours: Double // e.g., 8.0 hours for "working"
+}
