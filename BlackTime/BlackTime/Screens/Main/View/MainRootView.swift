e5af968507f2f19c77283adabbdc22b9979eed26
Arsenty Halimovsky <ghost12@yandex.ru>
2025-01-30 09:04:49 +0300

    Setup timeChartView

--- BlackTime/BlackTime/Screens/Main/View/MainRootView.swift
+++ BlackTime/BlackTime/Screens/Main/View/MainRootView.swift
@@ -1,17 +1,82 @@
 import UIKit
+import SnapKit
 
 /// `root View` of the Main Screen
 final class MainRootView: UIView {
+
+	// MARK: - Properties
+
+	let timeChartView = CircularTimeChartView()
 
 	// MARK: - Initialization
 
 	init() {
 		super.init(frame: .zero)
-		backgroundColor = AppColors.primaryColor
-		print("got root view")
+
+		embedViews()
+		setupLayout()
+		setupAppearance()
+		setupData()
+		setupBehaviour()
+
 	}
 
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+// MARK: - Private Methods
+
+// MARK: - Embed Views
+
+private extension MainRootView {
+
+	func embedViews() {
+
+		addSubview(timeChartView)
+	}
+}
+
+// MARK: - Setup Layout
+
+private extension MainRootView {
+
+	func setupLayout() {
+
+		timeChartView.snp.makeConstraints { make in
+			make.width.equalTo(300)
+			make.height.equalTo(300)
+			make.center.equalToSuperview()
+		}
+	}
+}
+
+// MARK: - Setup Appearance
+
+private extension MainRootView {
+
+	func setupAppearance() {
+
+		backgroundColor = AppColors.primaryColor
+
+	}
+}
+
+// MARK: - Setup Data
+
+private extension MainRootView {
+
+	func setupData() {
+
+	}
+}
+
+// MARK: - Setup Behaviour
+
+private extension MainRootView {
+
+	func setupBehaviour() {
+		
 	}
 }
