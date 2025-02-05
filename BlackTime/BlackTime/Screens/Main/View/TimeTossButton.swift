955c7ed5d8a16cef8468297224d49098585a0567
Arsenty Halimovsky <ghost12@yandex.ru>
2025-02-05 09:54:36 +0300

    Remove RadialMenuView

--- BlackTime/BlackTime/Screens/Main/View/TimeTossButton.swift
+++ BlackTime/BlackTime/Screens/Main/View/TimeTossButton.swift
@@ -6,8 +6,8 @@ final class TimeTossButton: UIButton {
 
 	// MARK: - Properties
 
-	/// A menu of time spending categories becomes visible when you drag `TimeTossButton`
-	let radialMenu = RadialMenuView()
+	/// Original position of the `TossButton`
+	var originalPosition: CGPoint = .zero
 
 	/// An options list to choose time interval for a new time spending operation. Becomes visible when you tap `TimeTossButton`
 	let optionsList = OptionsListView()
@@ -20,7 +20,9 @@ final class TimeTossButton: UIButton {
 
 	/// Property to track when user dragged the "coin"
 	var isDragging = false
-	
+
+	/// A menu of time spending categories becomes visible when you drag `TimeTossButton`
+	// var radialMenu: RadialMenuView?
 
 	// MARK: - Initialization
 
@@ -38,6 +40,29 @@ final class TimeTossButton: UIButton {
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
+
+	// MARK: - Life Cycle
+
+	override func didMoveToSuperview() {
+		super.didMoveToSuperview()
+		if let superview = superview {
+			originalPosition = center  // Capture initial position
+		}
+	}
+
+	// MARK: - Hit Test
+
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		super.touchesEnded(touches, with: event)
+		print("touches ended")
+		isDragging = false
+	}
+
+	override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+		super.touchesCancelled(touches, with: event)
+		print("touches canceled")
+		isDragging = false
+	}
 }
 
 // MARK: - Private Methods
@@ -48,7 +73,6 @@ private extension TimeTossButton {
 
 	func embedViews() {
 
-		addSubview(radialMenu)
 		addSubview(optionsList)
 	}
 }
@@ -97,14 +121,37 @@ private extension TimeTossButton {
 
 	func setupBehaviour() {
 
-		radialMenu.isHidden = true
 		optionsList.isHidden = true
 
 		addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
 
 		addTarget(self, action: #selector(buttonDragged), for: .touchDragInside)
 
+		// In TimeTossButton's setupBehaviour()
+		let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
+		addGestureRecognizer(panGesture)
+	}
 
+	@objc func handlePan(_ gesture: UIPanGestureRecognizer) {
+			guard let superview = superview else { return }
+			let translation = gesture.translation(in: superview)
+
+			switch gesture.state {
+			case .changed:
+				center = CGPoint(x: center.x + translation.x, y: center.y + translation.y)
+				gesture.setTranslation(.zero, in: superview)
+				// Do not show radial menu here
+			case .ended:
+				isDragging = false
+				resetPosition()
+			default: break
+			}
+		}
+
+	private func resetPosition() {
+		UIView.animate(withDuration: 0.3) {
+			self.center = self.originalPosition  // Reset to original position
+		}
 	}
 
 	@objc func buttonTapped() {
@@ -128,79 +175,6 @@ private extension TimeTossButton {
 
 	@objc func buttonDragged() {
 		isDragging = true
-
-		if isDragging {
-			radialMenu.isHidden = false
-			print("radial menu is visible")
-		} else {
-			radialMenu.isHidden = true
-			print("radial menu is hidden")
-		}
 	}
+
 }
-
-
-
-/*
-import UIKit
-
-class CustomButton: UIButton {
-	var optionsView: UIView!
-	var isOptionsVisible = false
-
-	override init(frame: CGRect) {
-		super.init(frame: frame)
-		setupButton()
-	}
-
-	required init?(coder: NSCoder) {
-		super.init(coder: coder)
-		setupButton()
-	}
-
-	private func setupButton() {
-		// Configure button appearance
-		self.backgroundColor = .systemBlue
-		self.layer.cornerRadius = 25
-		self.setTitle("+", for: .normal)
-		self.titleLabel?.font = UIFont.systemFont(ofSize: 24)
-
-		// Add tap action
-		self.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-
-		// Setup options view
-		optionsView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
-		optionsView.backgroundColor = .lightGray
-		optionsView.layer.cornerRadius = 10
-		optionsView.isHidden = true
-		self.addSubview(optionsView)
-
-		// Add pan gesture for drag and drop
-		let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-		self.addGestureRecognizer(panGesture)
-
-	}
-
-	@objc private func buttonTapped() {
-		isOptionsVisible.toggle()
-		UIView.animate(withDuration: 0.3) {
-			self.optionsView.isHidden = !self.isOptionsVisible
-		}
-	}
-
-	@objc private func handlePan(gesture: UIPanGestureRecognizer) {
-		let translation = gesture.translation(in: self)
-		self.center = CGPoint(x: self.center.x + translation.x, y: self.center.y + translation.y)
-		gesture.setTranslation(.zero, in: self)
-
-		// Implement hit testing and category selection logic here
-	}
-
-	@objc private func handleLongPress(gesture: UILongPressGestureRecognizer) {
-		if gesture.state == .began {
-			// Switch mode or time duration
-			print("Long press detected")
-		}
-	}
-}
- */
