cb9d23d53ac61f80c3bb99c17711269747bf46e9
Arsenty Halimovsky <ghost12@yandex.ru>
2025-02-05 09:54:44 +0300

    Edit RadialMenuView

--- BlackTime/BlackTime/Screens/Main/View/RadialMenu.swift
+++ BlackTime/BlackTime/Screens/Main/View/RadialMenu.swift
@@ -1,148 +1,69 @@
 import UIKit
 import SnapKit
 
-
+/// Custom class to implement a Radial Menu when you drag the "+" button
 final class RadialMenuView: UIView {
 
-	var buttons: [UIView] = []
+	// MARK: - Properties
+
+	var categoryViews: [UIView] = []
 	let radius: CGFloat = 100
 	let arcAngle: CGFloat = .pi / 2 // 90 degrees
-}
 
+	// MARK: - Initialization
 
-// MARK: - Private Methods
+	init(categories: [TimeCategory]) {
+			super.init(frame: .zero)
+			setupMenu(categories: categories)
+			isHidden = false
+		}
 
-// MARK: - Embed Views
-
-private extension RadialMenuView {
-
-	func embedViews() {
-
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
 	}
 }
 
-// MARK: - Setup Layout
+// MARK: - Public Methods
 
-private extension RadialMenuView {
+extension RadialMenuView {
+	func show() {
+		self.isHidden = false
+		UIView.animate(withDuration: 0.3) {
+			self.alpha = 1
+		}
+	}
 
-	func setupLayout() {
-
-
+	func hide() {
+		UIView.animate(withDuration: 0.3, animations: {
+			self.alpha = 0
+		}) { _ in
+			self.isHidden = true
+		}
 	}
 }
 
-// MARK: - Setup Appearance
+extension RadialMenuView {
 
-private extension RadialMenuView {
+	func setupMenu(categories: [TimeCategory]) {
+		for category in categories {
+			let label = UILabel()
+			label.text = category.name
+			label.backgroundColor = category.color
+			label.layer.cornerRadius = 25
+			label.clipsToBounds = true
+			categoryViews.append(label)
+			addSubview(label)
+		}
+		layoutButtons(center: center)
+	}
 
-	func setupAppearance() {
-
+	func layoutButtons(center: CGPoint) {
+		let angleIncrement = arcAngle / CGFloat(categoryViews.count - 1)
+		for (index, label) in categoryViews.enumerated() {
+			let angle = -arcAngle / 2 + angleIncrement * CGFloat(index)
+			let x = center.x + radius * cos(angle)
+			let y = center.y + radius * sin(angle)
+			label.frame = CGRect(x: x, y: y, width: 50, height: 50)
+		}
 	}
 }
-
-// MARK: - Setup Data
-
-private extension RadialMenuView {
-
-	func setupData() {
-
-	}
-}
-
-// MARK: - Setup Behaviour
-
-private extension TimeTossButton {
-
-	func setupBehaviour() {
-
-
-	}
-}
-
-/*
- // RadialMenuView.swift
- class RadialMenuView: UIView {
-	 var buttons: [UIButton] = []
-	 let radius: CGFloat = 100
-	 let arcAngle: CGFloat = .pi / 2 // 90 degrees
-
-	 init(options: [String], center: CGPoint) {
-		 super.init(frame: .zero)
-		 setupMenu(options: options, center: center)
-	 }
-
-	 required init?(coder: NSCoder) {
-		 fatalError("init(coder:) has not been implemented")
-	 }
-
-	 private func setupMenu(options: [String], center: CGPoint) {
-		 for (index, option) in options.enumerated() {
-			 let button = UIButton(type: .system)
-			 button.setTitle(option, for: .normal)
-			 button.backgroundColor = .systemBlue
-			 button.layer.cornerRadius = 20
-			 buttons.append(button)
-			 self.addSubview(button)
-		 }
-		 layoutButtons(center: center)
-	 }
-
-	 private func layoutButtons(center: CGPoint) {
-		 let angleIncrement = arcAngle / CGFloat(buttons.count - 1)
-		 for (index, button) in buttons.enumerated() {
-			 let angle = -arcAngle / 2 + angleIncrement * CGFloat(index)
-			 let x = center.x + radius * cos(angle)
-			 let y = center.y + radius * sin(angle)
-			 button.frame = CGRect(x: x, y: y, width: 40, height: 40)
-		 }
-	 }
-
-	 func show() {
-		 self.isHidden = false
-		 UIView.animate(withDuration: 0.3) {
-			 self.alpha = 1
-		 }
-	 }
-
-	 func hide() {
-		 UIView.animate(withDuration: 0.3, animations: {
-			 self.alpha = 0
-		 }) { _ in
-			 self.isHidden = true
-		 }
-	 }
- }
-
- // TimeTossButton.swift
- class TimeTossButton: UIButton {
-	 var radialMenu: RadialMenuView?
-
-	 override init(frame: CGRect) {
-		 super.init(frame: frame)
-		 setupButton()
-	 }
-
-	 required init?(coder: NSCoder) {
-		 super.init(coder: coder)
-		 setupButton()
-	 }
-
-	 private func setupButton() {
-		 self.backgroundColor = .systemBlue
-		 self.layer.cornerRadius = 25
-		 self.setTitle("+", for: .normal)
-		 self.titleLabel?.font = UIFont.systemFont(ofSize: 24)
-
-		 // Add tap action to show radial menu
-		 self.addTarget(self, action: #selector(handleTap), for: .touchUpInside)
-	 }
-
-	 @objc private func handleTap() {
-		 if radialMenu == nil {
-			 radialMenu = RadialMenuView(options: ["Option 1", "Option 2", "Option 3"], center: self.center)
-			 self.superview?.addSubview(radialMenu!)
-		 }
-		 radialMenu?.show()
-	 }
- }
- */
